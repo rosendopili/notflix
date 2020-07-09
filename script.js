0708dd67f3094d703477b919cd7455b755860c3f
rosendopili <rosendopili@gmail.com>
2020-07-08 22:28:34 -0400

    adding latest changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const searchButton = document.querySelector('.searchButton'); 
 const resetButton = document.querySelector('.resetButton'); 
 const cardTemplate = document.querySelector('template'); 
-const list = document.querySelector('.container'); 
+const searchContainer = document.querySelector('.searchContainer');
+const fillContainer = document.querySelector('.fillContainer');  
 const error = document.querySelector('.error'); 
 const nextButton = document.querySelector('.nextButton'); 
 const prevButton = document.querySelector('.prevButton'); 
@@ -9,12 +10,25 @@ const search = document.querySelector('.search');
 const form = document.querySelector('form'); 
 
 let page = 1; 
+let category = "x-men"
+
+fetch(`https://www.omdbapi.com/?s=${category}&apikey=c0b965ad`)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            showMovies(response.Search, fillContainer)
+            console.log(response); 
+        })
+        .catch(err => {
+            console.log(err);
+        });    
 
 
 const getData = (e) => {
     e.preventDefault(); 
     console.log(page); 
-    list.textContent = ""; 
+    searchContainer.textContent = ""; 
     error.textContent = "";
     error.classList.add('display-none');  
     let query = search.value; 
@@ -23,9 +37,7 @@ const getData = (e) => {
             return response.json();
         })
         .then(response => {
-            console.log(response); 
-            console.log(response.totalResults); 
-            showMovies(response.Search); 
+            showMovies(response.Search, searchContainer); 
         })
         .catch(err => {
             console.log(err);
@@ -35,13 +47,13 @@ const getData = (e) => {
 }
 
 
-const showMovies = (data) => {
+const showMovies = (data, parent) => {
 
     for (let i = 0; i < data.length; i ++){
 
         const listItem = document.createElement('div'); 
-        listItem.classList.add('movieCard'); 
-        list.appendChild(listItem); 
+        listItem.classList.add('movieCard');
+        parent.appendChild(listItem); 
 
         const cardImage = document.createElement('img'); 
         listItem.appendChild(cardImage); 
@@ -50,15 +62,14 @@ const showMovies = (data) => {
         listItem.appendChild(cardTitle); 
     
         cardTitle.innerHTML = data[i].Title; 
+
         if(data[i].Poster === "N/A"){
             cardImage.setAttribute('src', './assets/movie-poster.jpg'); 
         }else{
             cardImage.setAttribute('src', data[i].Poster);
         }
     }
-
 }
-
 
 searchButton.addEventListener('click', (e) =>{
     e.preventDefault()
@@ -67,8 +78,12 @@ searchButton.addEventListener('click', (e) =>{
 
 nextButton.addEventListener('click', (e) => {
     e.preventDefault; 
-  console.log(error.textContent); 
-    page += 1;
+    console.log(error.textContent); 
+    if(error.textContent === "No results"){
+        page = 1; 
+    }else{
+        page += 1;
+    }
     getData(e); 
 })
 
